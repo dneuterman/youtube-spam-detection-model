df9f85bd53280bc01d4ccff5f008d79fc4cc583c
dneuterman <dneuterman@gmail.com>
2023-12-28 21:50:12 -0500

    Creates navigation.html template. Displays plots on index page.

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def index():
-    return "<p>Index Page</p>"
-
-@app.route("/hello/")
-def hello():
     return render_template('index.html')
