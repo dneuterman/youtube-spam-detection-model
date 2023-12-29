d0d5a0adb5ab1ffbe2e7e9fe06923ef9e64754ff
dneuterman <dneuterman@gmail.com>
2023-12-28 23:34:55 -0500

    Creates funnctions to convert dataset to json for gui display.

--- detection-model.py
+++ detection-model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re
+import json
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -19,11 +20,11 @@ from sklearn.pipeline import Pipeline
 from sklearn import metrics
 
 #imports data from csv dataset
-comments1 = pd.read_csv("./datasets/Youtube01-Psy.csv", encoding = "utf-8")
-comments2 = pd.read_csv("./datasets/Youtube02-KatyPerry.csv", encoding = "utf-8")
-comments3 = pd.read_csv("./datasets/Youtube03-LMFAO.csv", encoding = "utf-8")
-comments4 = pd.read_csv("./datasets/Youtube04-Eminem.csv", encoding = "utf-8")
-comments5 = pd.read_csv("./datasets/Youtube05-Shakira.csv", encoding = "utf-8")
+comments1 = pd.read_csv("./datasets/raw/Youtube01-Psy.csv", encoding = "utf-8")
+comments2 = pd.read_csv("./datasets/raw/Youtube02-KatyPerry.csv", encoding = "utf-8")
+comments3 = pd.read_csv("./datasets/raw/Youtube03-LMFAO.csv", encoding = "utf-8")
+comments4 = pd.read_csv("./datasets/raw/Youtube04-Eminem.csv", encoding = "utf-8")
+comments5 = pd.read_csv("./datasets/raw/Youtube05-Shakira.csv", encoding = "utf-8")
 
 #combines csv datasets and removes unnecessary columns
 comments_dataframe = pd.concat([comments1, comments2, comments3, comments4, comments5], ignore_index=True)
@@ -41,6 +42,28 @@ plt.savefig("./static/plots/comments-classification-pie-chart.svg")
 
 #splits dataset into training and testing set 70/30
 comments_train_split, comments_test_split, spam_classification_train, spam_classification_test = train_test_split(comments_dataframe["COMMENT"], comments_dataframe["SPAM_CLASSIFICATION"], test_size=0.3, random_state=31)
+
+def convert_initial_to_json(split_comments_series, clean=False):
+    comments_index_list = split_comments_series.index.tolist()
+    comments_array = []
+    for index in comments_index_list:
+        if clean == True:
+            current_comment = " ".join(preprocess_comments(comments_dataframe["COMMENT"][index]))
+        else:
+            current_comment = comments_dataframe["COMMENT"][index]
+        comment_info = {
+            "comment_id": comments_dataframe["COMMENT_ID"][index],
+            "comment": current_comment,
+            "actual_classification": int(comments_dataframe["SPAM_CLASSIFICATION"][index]),
+            "predicted_classification": "null"
+        }
+        comments_array.append(comment_info)
+    return comments_array
+
+def save_dataset_to_json(data, filename):
+    with open(f"./datasets/json/{filename}", "w") as file:
+        json.dump(data, file)
+
 
 #function to remove stop words, punctuation, numbers and URLs from comment
 def preprocess_comments(comment):
@@ -90,6 +113,11 @@ def wordcloud_generator(word_freq_dict, wordcloud_filename):
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
     plt.savefig(f"./static/plots/{wordcloud_filename}.svg")
+
+save_dataset_to_json(convert_initial_to_json(comments_train_split), "training-comments-dataset.json")
+save_dataset_to_json(convert_initial_to_json(comments_train_split, clean=True), "clean-training-comments-dataset.json")
+save_dataset_to_json(convert_initial_to_json(comments_test_split), "test-comments-dataset.json")
+save_dataset_to_json(convert_initial_to_json(comments_test_split, clean=True), "clean-test-comments-dataset.json")
 
 spam_train_words_freq = word_frequency_generator(comments_train_split, spam_classification_train, 1)
 wordcloud_generator(spam_train_words_freq, "spam-words-training-wordcloud")
