5ea9d59b62d71cdde5cb9fcb37d732951bac743d
dneuterman <dneuterman@gmail.com>
2024-01-04 23:10:39 -0500

    Reduces needed json files during initialization

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ import random
 
 with open("./datasets/json/training-comments-dataset.json", "r") as file:
     training_comments_json = json.load(file)
-with open("./datasets/json/clean-training-comments-dataset.json", "r") as file:
-    clean_training_comments_json = json.load(file)
 with open("./datasets/json/test-comments-dataset.json", "r") as file:
     test_comments_json = json.load(file)
-with open("./datasets/json/clean-test-comments-dataset.json", "r") as file:
-    clean_test_comments_json = json.load(file)
 
 app = Flask(__name__)
 
@@ -22,14 +18,12 @@ def dataset():
     sample_comment = ""
     clean_sample_comment = ""
     sample_comment_classification = ""
-    clean_sample_comment_classification = ""
     while(clean_sample_comment == ""):
         rand_comment = random.randrange(0, len(training_comments_json))
         sample_comment = training_comments_json[rand_comment]["comment"]
-        clean_sample_comment = clean_training_comments_json[rand_comment]["comment"]
+        clean_sample_comment = training_comments_json[rand_comment]["clean_comment"]
         sample_comment_classification = str(training_comments_json[rand_comment]["actual_classification"])
-        clean_sample_comment_classification = str(clean_training_comments_json[rand_comment]["actual_classification"])
-    return render_template('dataset.html', sample_comment = sample_comment, clean_sample_comment = clean_sample_comment, sample_comment_classification = sample_comment_classification, clean_sample_comment_classification = clean_sample_comment_classification)
+    return render_template('dataset.html', sample_comment = sample_comment, clean_sample_comment = clean_sample_comment, sample_comment_classification = sample_comment_classification)
 
 @app.route("/training/")
 def training():
--- detection_model.py
+++ detection_model.py
@@ -42,19 +42,16 @@ plt.savefig("./static/plots/comments-classification-pie-chart.svg")
 #splits dataset into training and testing set 70/30
 comments_train_split, comments_test_split, spam_classification_train, spam_classification_test = train_test_split(comments_dataframe["COMMENT"], comments_dataframe["SPAM_CLASSIFICATION"], test_size=0.3, random_state=31)
 
-def convert_initial_to_json(split_comments_series, clean=False):
+def convert_initial_to_json(split_comments_series):
     comments_index_list = split_comments_series.index.tolist()
     comments_array = []
     for index in comments_index_list:
-        if clean == True:
-            current_comment = " ".join(comments_preprocessor(comments_dataframe["COMMENT"][index]))
-        else:
-            current_comment = comments_dataframe["COMMENT"][index]
+        current_comment = comments_dataframe["COMMENT"][index]
         comment_info = {
             "comment_id": comments_dataframe["COMMENT_ID"][index],
             "comment": current_comment,
-            "actual_classification": int(comments_dataframe["SPAM_CLASSIFICATION"][index]),
-            "predicted_classification": "null"
+            "clean_comment": " ".join(comments_preprocessor(current_comment)),
+            "actual_classification": int(comments_dataframe["SPAM_CLASSIFICATION"][index])
         }
         comments_array.append(comment_info)
     return comments_array
@@ -81,10 +78,15 @@ def wordcloud_generator(word_freq_dict, wordcloud_filename):
     plt.axis("off")
     plt.savefig(f"./static/plots/{wordcloud_filename}.svg")
 
+#creates small sample from test dataset to use for later
+def create_sample_test_dataset(test_dataset):
+    pass
+
+comments_train_split_json = convert_initial_to_json(comments_train_split)
+comments_test_split_json = convert_initial_to_json(comments_test_split)
+
 save_dataset_to_json(convert_initial_to_json(comments_train_split), "training-comments-dataset.json")
-save_dataset_to_json(convert_initial_to_json(comments_train_split, clean=True), "clean-training-comments-dataset.json")
 save_dataset_to_json(convert_initial_to_json(comments_test_split), "test-comments-dataset.json")
-save_dataset_to_json(convert_initial_to_json(comments_test_split, clean=True), "clean-test-comments-dataset.json")
 
 spam_train_words_freq = word_frequency_generator(comments_train_split, spam_classification_train, 1)
 wordcloud_generator(spam_train_words_freq, "spam-words-training-wordcloud")
