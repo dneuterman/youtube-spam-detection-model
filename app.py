65be776e5e49f94b24f91ab6c18cb60623a3a17c
dneuterman <dneuterman@gmail.com>
2024-01-06 23:10:17 -0500

    Adds ability to predict submitted comment and sample comments.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import random
+from pipeline_processor import PipelineProcessor
 
 with open("./datasets/json/training-comments-dataset.json", "r") as file:
     training_comments_json = json.load(file)
@@ -8,6 +9,8 @@ with open("./datasets/json/test-comments-dataset.json", "r") as file:
     test_comments_json = json.load(file)
 with open("./datasets/json/sample-comments.json", "r") as file:
     sample_comments_json = json.load(file)
+
+mnb_pipeline = PipelineProcessor()
 
 app = Flask(__name__)
 
@@ -35,12 +38,21 @@ def training():
 def prediction():
     if request.method == "POST":
         result = []
+        comments_to_predict = []
         if "user-comment-btn" in request.form:
             result.append({
                 "comment": request.form.get('comment')
             })
         if "sample-comments-btn" in request.form:
             result = sample_comments_json
+
+        for i in range(len(result)):
+            comments_to_predict.append(result[i]["comment"])
+
+        mnb_pipeline.predict_model(comments_to_predict)
+        for i in range(len(result)):
+            result[i]["predicted_classification"] = mnb_pipeline.prediction[i]
+
         return render_template('completed-prediction.html', result = result)
     else:
         return render_template('prediction.html', sample_comments = sample_comments_json)
