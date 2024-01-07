d9f11808f93226bf65a082ae4d086b55eb9f260f
dneuterman <dneuterman@gmail.com>
2024-01-07 02:40:43 -0500

    Adds download-comments.html. Saves comments_to_delete.json to folder

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import random
+import os
 from pipeline_processor import PipelineProcessor
 
 with open("./datasets/json/training-comments-dataset.json", "r") as file:
@@ -10,7 +11,18 @@ with open("./datasets/json/test-comments-dataset.json", "r") as file:
 with open("./datasets/json/sample-comments.json", "r") as file:
     sample_comments_json = json.load(file)
 
+class PredictedComments:
+    def __init__(self):
+        self.comments = None
+
+    def get_comments(self):
+        return self.comments
+    
+    def set_comments(self, comments):
+        self.comments = comments
+
 mnb_pipeline = PipelineProcessor()
+predicted_comments = PredictedComments()
 
 app = Flask(__name__)
 
@@ -37,6 +49,22 @@ def training():
 @app.route("/prediction/", methods = ["GET", "POST"])
 def prediction():
     if request.method == "POST":
+        if "download-spam-comments" in request.form:
+            comments_to_delete = []
+            comments = predicted_comments.get_comments()
+            for i in range(len(comments)):
+                if comments[i]["predicted_classification"] == 1:
+                    del comments[i]["clean_comment"]
+                    comments_to_delete.append(comments[i])
+
+            filename = "comments_to_delete.json"
+
+            with open(f"./datasets/saved/{filename}", "w") as file:
+                json.dump(comments_to_delete, file)
+
+            filepath = f"{os.path.dirname(app.instance_path)}\datasets\saved\{filename}"
+
+            return render_template('download-comments.html', filepath = filepath)
         result = []
         comments_to_predict = []
         if "user-comment-btn" in request.form:
@@ -51,8 +79,8 @@ def prediction():
 
         mnb_pipeline.predict_model(comments_to_predict)
         for i in range(len(result)):
-            result[i]["predicted_classification"] = mnb_pipeline.prediction[i]
-
+            result[i]["predicted_classification"] = int(mnb_pipeline.prediction[i])
+        predicted_comments.set_comments(result)
         return render_template('completed-prediction.html', result = result)
     else:
         return render_template('prediction.html', sample_comments = sample_comments_json)
